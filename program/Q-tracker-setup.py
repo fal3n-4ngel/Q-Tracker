982ae1000889b86195d185b71cda7449e52c107b
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2022-02-24 09:50:46 +0530

    Update Q-tracker-setup.py

--- program/Q-tracker-setup.py
+++ program/Q-tracker-setup.py
@@ -8,9 +8,9 @@ cursor=mycon.cursor(buffered=True)
 cmd1='create database qtracker'
 cmd2='use qtracker'
 cmd3='create table corona(id varchar(10),name varchar(25),loc varchar(10),address varchar(30),phone varchar(10),date date,time time,result varchar(10))'
-cmd3='create table id(count int)'
-cmd4='insert into id values(0)'
-licmd=[cmd1,cmd2,cmd3,cmd4]
+cmd4='create table id(count int)'
+cmd5='insert into id values(0)'
+licmd=[cmd1,cmd2,cmd3,cmd4,cmd5]
 for i in licmd:
 	cursor.execute(i)
 	mycon.commit()
